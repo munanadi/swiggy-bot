ee2e5e7a7b144febe39832c87f9728e4fdca357e
nana <ajibaa@gmail.com>
2023-06-04 03:40:49 +0530

    openai transcript works

--- README.md
+++ README.md
@@ -8,3 +8,4 @@ This should get Youtube, Instagram reel videos of cooking, baking etc and figure
 
 1. Setup twilio account and connect sandbox
 2. run local node, and tunnel to expose to public internet (using ngork or cloudlfared)
+3. Set this exposed localhost in your sandbox settings in twilio
--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const { twiml } = require("twilio");
 const { MessagingResponse } = twiml;
 const { Configuration, OpenAIApi } = require("openai");
 require("dotenv").config();
+const fs = require("fs");
+const ytdl = require("ytdl-core");
+
+// Connect to the Openai API and send the message forward.
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
 // Create an Express application
 const app = express();
@@ -13,37 +20,92 @@ app.use(urlencoded({ extended: false }));
 
 // Define a route to handle incoming messages from Twilio
 app.post("/webhook", async (req, res) => {
-  console.log(req.body);
-
   const message = req.body.Body;
   const sender = req.body.From;
   const profileName = req.body.ProfileName;
 
-  // Process the incoming message and generate a response
-  const response = await generateResponse(
-    message,
-    profileName
-  );
+  const youtubeRegex =
+    /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/gm;
+
+  const match = youtubeRegex.exec(message);
+
+  let response;
+
+  // Found YT Url in message
+  if (match) {
+    const youtubeId = match[7];
+    response = await handleYoutubeLinks(youtubeId);
+  } else {
+    // Process the incoming message and generate a response
+    response = await generateResponse(message, profileName);
+  }
 
   // Send the response back to the user
   const twiml = new MessagingResponse();
   twiml.message(response);
 
+  console.log(response.slice(0, 100));
+  console.log(twiml.toString());
+
   res.set("Content-Type", "text/xml");
   res.send(twiml.toString());
 });
 
+async function handleYoutubeLinks(youtubeId) {
+  const openai = new OpenAIApi(configuration);
+  // Download the Youtube video.
+  try {
+    await new Promise((resolve) => {
+      ytdl(`http://www.youtube.com/watch?v=${youtubeId}`, {
+        filter: "audioonly",
+        quality: "highestaudio",
+      })
+        .pipe(fs.createWriteStream("audio.mp3"))
+        .on("close", () => {
+          resolve();
+        });
+    });
+  } catch (e) {
+    console.log(`Error Transcribing the file`, e);
+  }
+
+  console.log(
+    "Done downloading the youtube audio!, Creating transcript"
+  );
+
+  try {
+    const resp = await openai.createTranscription(
+      fs.createReadStream("audio.mp3"),
+      "whisper-1",
+      "",
+      undefined,
+      0,
+      "en"
+    );
+
+    // Create the transcipt file locally
+    fs.writeFileSync(
+      "transctipt.txt",
+      resp.data.text.toString()
+    );
+
+    console.log("Transctipt done");
+
+    const transcript = resp.data.text;
+
+    return transcript;
+  } catch (e) {
+    console.log(e);
+    return "None";
+  }
+}
+
 // Function to generate a response based on the received message
 async function generateResponse(message, profileName) {
-  // Connect to the Openai API and send the message forward.
-  const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-  });
-
   const openai = new OpenAIApi(configuration);
   const response = await openai.createCompletion({
     model: "text-davinci-003",
-    prompt: `This is what the user said : ${message}. 
+    prompt: `This is what the user said : ${message}.
     Give a short introduction about yourself in two lines and continue the conversation ${profileName}`,
     max_tokens: 500,
     temperature: 0,
@@ -54,9 +116,9 @@ async function generateResponse(message, profileName) {
   // Example: Echo back the received message
   // return `You said: ${message}`;
   return `You said: ${message}
-${response.data.choices[0].text}
+  ${response.data.choices[0].text}
 
-${response.data.usage.total_tokens} tokens were used!`;
+  ${response.data.usage.total_tokens} tokens were used!`;
 }
 
 // Start the server
